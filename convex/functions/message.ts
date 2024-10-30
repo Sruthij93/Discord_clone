7252de78a52726b361dbe9d7e39bef1b32fbf620
Sruthi <sjayachandran@nevada.unr.edu>
2024-10-29 20:21:04 -0700

    Initialized Convex with messages API

---
+++ convex/functions/message.ts
@@ -0,0 +1,19 @@
+// define functions that can be used to interact with the messages table
+import { mutation, query } from "../_generated/server";
+import { v } from "convex/values";
+
+export const list = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("messages").collect();
+  },
+});
+
+export const create = mutation({
+  args: {
+    sender: v.string(),
+    content: v.string(),
+  },
+  handler: async (ctx, { sender, content }) => {
+    await ctx.db.insert("messages", { sender, content });
+  },
+});
